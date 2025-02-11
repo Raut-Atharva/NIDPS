75dac7c6b6a3355f75cdc2a050507cf5b1ad40b6
Your Name <you@example.com>
2025-02-11 14:36:13 +0530

    Updated the footer

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="p-6  text-center">
       <div className="container mx-auto">
-        <p className="mb-4">&copy; 2024 NetworkScout. All rights reserved.</p>
+        <p className="mb-4">&copy; 2025 NetworkScout. All rights reserved.</p>
         <p>Protecting your network with cutting-edge technology.</p>
       </div>
     </footer>
